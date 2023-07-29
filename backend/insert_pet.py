8dc0bc336bc9946ea337df16d48fbae0ab81be41
writer-mng <volodymyrpetriv2207@gmail.com>
2023-07-29 21:44:47 +0300

    Inserted data about pet projects for first 10 jobs

--- backend/insert_pet.py
+++ backend/insert_pet.py
@@ -1,11 +1,14 @@
+import os
 import json
+from peewee import SqliteDatabase
 from database import PetProject
 
 
-with open("backend/pet.json", encoding="utf-8") as file:
+with open(os.path.realpath(__file__).replace("insert_pet.py", "pet.json", encoding="utf-8")) as file:
     pets_data = json.load(file)
     for data in pets_data.keys():
         for pet in pets_data[data]:
+            print(pet)
             row, created = PetProject.get_or_create(
                 name=pet["name"],
                 description=pet["description"],
