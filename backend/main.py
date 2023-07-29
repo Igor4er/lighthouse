ffae661ad2b61e4e39a1d04650baaa171791ce8c
writer-mng <volodymyrpetriv2207@gmail.com>
2023-07-29 15:00:34 +0300

    Perfected endpoint that return specialities

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,32 @@
 from fastapi import FastAPI, Depends
+import json
 from peewee import SqliteDatabase
-from transliterate import translit
+from playhouse.shortcuts import model_to_dict
 from database import get_db, Speciality
-
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get(path="/get/")
-def get_db(db: SqliteDatabase = Depends(get_db)):
+def get_data():
     try:
-        user = Speciality.select()
-        return user
+        data = Speciality.select()
+        data_list = []
+        for d in data:
+            data_list.append({"id": d.id, "name": d.name, "transliterated": d.transliterated})
+        return data_list
+
     except Speciality.DoesNotExist:
         return "Sir, seems like we don't have no data. Would You like ask my developer to fetch some for you. Thank You, have a great time"
-    return {"Hello": mess, "string": string}
 
+    return {"Hello": "world"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 @app.get(path="/spec/")
 def insert_spec(name:str):
-    ua_name = translit(name, 'uk')
-    return {"Eng": name, "Ua": ua_name}
+    return {"Eng": name}
