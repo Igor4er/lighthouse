be1191ecdd7c0ed276777135652f269a038108a6
Igor4er <sherednihenko.ihor@gmail.com>
2023-07-30 11:24:21 +0300

    Works

--- front/src/routes/s/[id]/index.tsx
+++ front/src/routes/s/[id]/index.tsx
@@ -24,52 +24,40 @@ const loc = useLocation();
     <div class="huge_blok flex justify-around ">
       <div class="left_blok w-2/5 rounded-2xl bg-[#222a34]  mt-8">
         <div class="text-2xl text-white text-4xl text-center mt-8">{signal.value.specinfo.name}:</div>
+        {signal.value.specinfo.reqs.map((x: string) => (
+          <>
           <div class="card w-96 bg-base-100 shadow-xl  m-auto mt-4">
             <div class="card-body">
-              <h2 class="card-title">Card title!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <h2 class="card-title">{x}</h2>
+                  <p>If a dog chews shoes whose shoes does he choose?</p>
             </div>
-          </div>
-          <div class="card w-96 bg-base-100 shadow-xl m-auto mt-4">
-            <div class="card-body flex">
-              <ul>
-                <li class="text-lg pl-[31px] font-medium"><h2 class="card-title">Card title!</h2></li>
-              </ul>
-              
-              {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
             </div>
-          </div>
-          <div class="card w-96 bg-base-100 shadow-xl m-auto mt-4">
-            <div class="card-body">
-              <h2 class="card-title">Card title!</h2>
-              {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
-            </div>
-          </div>
+          </>
+          )) }
       </div>
-      <div class="right_blok w-2/5 rounded-2xl bg-[#222a34] mt-8">
-        <div class="card w-96 bg-[#F2C94C] text-primary-content m-auto">
-          <div class="card-body">
-            <h2 class="card-title">Card title!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-            </div>
-          </div>
-        </div>
-        </div>
-      </div>
-            {signal.value.specinfo.reqs.map((x: string) => (
-                <div key={x}> <div class="text-lg pl-[31px] font-medium">{x}</div> </div>  
-            )) }
-            <div class="text-3xl">PROJ:</div>
-            {signal.value.specinfo.projects.map((x: any) => (
-                <div key={x.name}> <div class="text-lg pl-[31px] font-medium">{x.name} <span class="bg-base-300">{x.desc}</span></div> </div>  
-            )) }
-            {userLoggedIn.value ?
-            <div>ПРЕМІУМ ВИДНО</div>
-            :
-            <div class="hidden">ПРЕМІУМ НЕ ВИДНО</div>
-            }
+      {userLoggedIn.value ?
+      <>
+        <div class="right_blok w-2/5 rounded-2xl bg-[#222a34] mt-8">
+          <div class="card w-96 bg-[#F2C94C] text-primary-content m-auto">
+          {signal.value.specinfo.projects.map((x: any) => (
+              <>
+                <div class="card-body" key={x.name}>
+                <h2 class="card-title">{x.name}</h2>
+                <p>{x.desc}</p>
+                <div class="card-actions justify-end"></div>
+                </div>
+              </>  
+              )) }
 
+          </div>
+          </div>
+      </>
+      :
+      <>
+        <div class="hidden">ПРЕМІУМ НЕ ВИДНО</div>
+      </>
+      }
+      </div>
     </>
   )
 });
